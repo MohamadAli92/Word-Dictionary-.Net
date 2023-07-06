51ef60959c3faf01d2ecb203db5bd3552640c89c
Mohammad Ali Kazemi <mohamadalikazemi92@gmail.com>
2023-07-07 00:03:25 +0430

    Some Changes.

--- MainForm.h
+++ MainForm.h
@@ -8,6 +8,7 @@ namespace DsProject {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace std;
 
 	/// <summary>
 	/// Summary for MainForm
@@ -34,6 +35,25 @@ namespace DsProject {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::TableLayoutPanel^ tableLayoutPanel1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::RichTextBox^ richTextBox1;
+	private: System::Windows::Forms::TableLayoutPanel^ tableLayoutPanel2;
+	private: System::Windows::Forms::FlowLayoutPanel^ flowLayoutPanel1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button5;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Button^ button3;
+
+	protected:
+
+
+
+	protected:
+
+
+	protected:
 
 	protected:
 
@@ -50,18 +70,207 @@ namespace DsProject {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->tableLayoutPanel1 = (gcnew System::Windows::Forms::TableLayoutPanel());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
+			this->tableLayoutPanel2 = (gcnew System::Windows::Forms::TableLayoutPanel());
+			this->flowLayoutPanel1 = (gcnew System::Windows::Forms::FlowLayoutPanel());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->tableLayoutPanel1->SuspendLayout();
+			this->tableLayoutPanel2->SuspendLayout();
+			this->flowLayoutPanel1->SuspendLayout();
 			this->SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			this->tableLayoutPanel1->ColumnCount = 2;
+			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
+				26.6129F)));
+			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
+				73.3871F)));
+			this->tableLayoutPanel1->Controls->Add(this->label1, 0, 0);
+			this->tableLayoutPanel1->Controls->Add(this->richTextBox1, 1, 0);
+			this->tableLayoutPanel1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->tableLayoutPanel1->Location = System::Drawing::Point(3, 3);
+			this->tableLayoutPanel1->Name = L"tableLayoutPanel1";
+			this->tableLayoutPanel1->RowCount = 1;
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 50)));
+			this->tableLayoutPanel1->Size = System::Drawing::Size(604, 161);
+			this->tableLayoutPanel1->TabIndex = 0;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(178)));
+			this->label1->Location = System::Drawing::Point(3, 0);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(154, 161);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Enter Text:";
+			this->label1->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// richTextBox1
+			// 
+			this->richTextBox1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->richTextBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(178)));
+			this->richTextBox1->Location = System::Drawing::Point(163, 3);
+			this->richTextBox1->Name = L"richTextBox1";
+			this->richTextBox1->Size = System::Drawing::Size(438, 155);
+			this->richTextBox1->TabIndex = 1;
+			this->richTextBox1->Text = L"";
+			this->richTextBox1->TextChanged += gcnew System::EventHandler(this, &MainForm::richTextBox1_TextChanged);
+			// 
+			// tableLayoutPanel2
+			// 
+			this->tableLayoutPanel2->ColumnCount = 1;
+			this->tableLayoutPanel2->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
+				100)));
+			this->tableLayoutPanel2->Controls->Add(this->tableLayoutPanel1, 0, 0);
+			this->tableLayoutPanel2->Controls->Add(this->flowLayoutPanel1, 0, 1);
+			this->tableLayoutPanel2->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->tableLayoutPanel2->Location = System::Drawing::Point(0, 0);
+			this->tableLayoutPanel2->Name = L"tableLayoutPanel2";
+			this->tableLayoutPanel2->RowCount = 2;
+			this->tableLayoutPanel2->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 54.15492F)));
+			this->tableLayoutPanel2->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 45.84507F)));
+			this->tableLayoutPanel2->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Absolute, 20)));
+			this->tableLayoutPanel2->Size = System::Drawing::Size(610, 310);
+			this->tableLayoutPanel2->TabIndex = 1;
+			this->tableLayoutPanel2->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &MainForm::tableLayoutPanel2_Paint);
+			// 
+			// flowLayoutPanel1
+			// 
+			this->flowLayoutPanel1->Controls->Add(this->button1);
+			this->flowLayoutPanel1->Controls->Add(this->button5);
+			this->flowLayoutPanel1->Controls->Add(this->button2);
+			this->flowLayoutPanel1->Controls->Add(this->button4);
+			this->flowLayoutPanel1->Controls->Add(this->button3);
+			this->flowLayoutPanel1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->flowLayoutPanel1->Location = System::Drawing::Point(3, 170);
+			this->flowLayoutPanel1->Name = L"flowLayoutPanel1";
+			this->flowLayoutPanel1->Size = System::Drawing::Size(604, 137);
+			this->flowLayoutPanel1->TabIndex = 1;
+			// 
+			// button1
+			// 
+			this->button1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->button1->Location = System::Drawing::Point(3, 3);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(154, 59);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this->button5->Location = System::Drawing::Point(163, 3);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(154, 59);
+			this->button5->TabIndex = 0;
+			this->button5->Text = L"button1";
+			this->button5->UseVisualStyleBackColor = true;
+			this->button5->Click += gcnew System::EventHandler(this, &MainForm::button5_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(323, 3);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(154, 59);
+			this->button2->TabIndex = 0;
+			this->button2->Text = L"button1";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(3, 68);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(154, 59);
+			this->button4->TabIndex = 0;
+			this->button4->Text = L"button1";
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(163, 68);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(154, 59);
+			this->button3->TabIndex = 0;
+			this->button3->Text = L"button1";
+			this->button3->UseVisualStyleBackColor = true;
 			// 
 			// MainForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(282, 253);
+			this->ClientSize = System::Drawing::Size(610, 310);
+			this->Controls->Add(this->tableLayoutPanel2);
 			this->Name = L"MainForm";
 			this->Text = L"MainForm";
+			this->Load += gcnew System::EventHandler(this, &MainForm::MainForm_Load);
+			this->tableLayoutPanel1->ResumeLayout(false);
+			this->tableLayoutPanel1->PerformLayout();
+			this->tableLayoutPanel2->ResumeLayout(false);
+			this->flowLayoutPanel1->ResumeLayout(false);
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
-	};
+	private: System::Void MainForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private:
+		//void OnMouseMove(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e);
+		//void OnCursorChanged(System::Object^ sender, System::EventArgs^ e);
+		//void OnMouseClick(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e);
+	private: System::Void tableLayoutPanel2_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
+	}
+private: System::Void button5_Click(System::Object^ sender, System::EventArgs^ e) {
 }
+private: System::Void richTextBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	int lastSpace = this->richTextBox1->Text->LastIndexOf(' ');
+	if (lastSpace < 0) lastSpace = 0;
+	button2->Text = lastSpace.ToString();
+	if (lastSpace >= 0) {
+		button1->Text = this->richTextBox1->Text->Substring(lastSpace);
+	}
+}
+};
+}
+
+
+//void DsProject::MainForm::OnMouseMove(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e)
+//{
+//	//throw gcnew System::NotImplementedException();
+//	int x = e->X;
+//	int y = e->Y;
+//
+//	this->textBox1->Text = x.ToString();
+//	this->textBox1->Text = y.ToString();
+//
+//}
+//
+//
+//void DsProject::MainForm::OnCursorChanged(System::Object^ sender, System::EventArgs^ e)
+//{
+//	throw gcnew System::NotImplementedException();
+//
+//	this->label1->Text = this->textBox1->Cursor->Position.X.ToString();
+//}
+//
+//
+//void DsProject::MainForm::OnMouseClick(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e)
+//{
+//	//throw gcnew System::NotImplementedException();
+//
+//	this->label1->Text = this->textBox1->
+//
+//	//this->label1->Text = this->textBox1->Text;
+//}
